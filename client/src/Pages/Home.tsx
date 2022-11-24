add17f5d3873f26574a5a62de579b3282b75e880
Verminus <caseycs80@gmail.com>
2022-11-24 18:50:04 +0700

    update Home

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -3,7 +3,19 @@ import React, { FC } from 'react';
 type HomeProps = {};
 
 const Home: FC<HomeProps> = (props) => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="avatar flex justify-center items-center h-full">
+        <div className="w-60 rounded-full">
+          <img src="https://placeimg.com/192/192/people" />
+        </div>
+      </div>
+
+      <div className="Start_Button mt-5 flex justify-center items-center">
+        <button className="btn btn-outline">Start</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
