aee2e780fcdda176ea385001f99896c06930d33a
Hansendt <razernol88885092@gmail.com>
2022-11-27 20:13:38 +0700

    Buat folder data\ishihara dan buat js

---
+++ client/src/data/ishihara-desc.js
@@ -0,0 +1,27 @@
+const ishihara = [
+    {
+        "id": 1,
+        "img" : "ishihara/1.jpg",
+        "answer" : "1",
+        "incorrect" : "kamu terindikasi buta warna ....",
+    },
+    {
+        "id": 2,
+        "img" : "ishihara/2.jpg",
+        "answer" : "1",
+        "incorrect" : "kamu terindikasi buta warna ....",
+    },
+    {
+        "id": 3,
+        "img" : "ishihara/3.jpg",
+        "answer" : "1",
+        "incorrect" : "kamu terindikasi buta warna ....",
+    },
+    {
+        "id": 4,
+        "img" : "ishihara/4.jpg",
+        "answer" : "1",
+        "incorrect" : "kamu terindikasi buta warna ....",
+    }
+
+]
