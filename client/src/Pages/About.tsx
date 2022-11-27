d9ed756a897ebdfffec67a3924f966dcfc656c6f
Hansendt <razernol88885092@gmail.com>
2022-11-27 23:14:56 +0700

    About us dan foto home

--- client/src/Pages/About.tsx
+++ client/src/Pages/About.tsx
@@ -2,6 +2,9 @@ import React, { FC } from 'react';
 import { Badge, Button, Card, Checkbox, Mask, Table } from 'react-daisyui';
 import hansen from '../assets/images/hansen_profile.jpg';
 import casey from '../assets/images/casey_profile.jpg';
+import filbert from '../assets/images/filbert_profile.jpg';
+import zidan from '../assets/images/zidan_profile.jpg'; 
+import timothy from '../assets/images/timothy_profile.jpg';
 
 type AboutProps = {};
 
@@ -40,9 +43,9 @@ const Home: FC<AboutProps> = (props) => {
 
                 <Table.Row>
                     <div className="flex items-center space-x-3 truncate">
-                    <Mask
+                    <Mask className="w-14"
                         variant="squircle"
-                        src="http://daisyui.com/tailwind-css-component-profile-2@56w.png"
+                        src= {filbert}
                     />
                     <div>
                         <div className="font-bold">Filbert Mangiri</div>
@@ -80,9 +83,9 @@ const Home: FC<AboutProps> = (props) => {
 
                 <Table.Row>
                     <div className="flex items-center space-x-3 truncate">
-                    <Mask
+                    <Mask className="w-14"
                         variant="squircle"
-                        src="http://daisyui.com/tailwind-css-component-profile-2@56w.png"
+                        src={zidan}
                     />
                     <div>
                         <div className="font-bold">Hillario Zidan</div>
@@ -100,17 +103,17 @@ const Home: FC<AboutProps> = (props) => {
 
                 <Table.Row>
                     <div className="flex items-center space-x-3 truncate">
-                    <Mask
+                    <Mask className="w-14"
                         variant="squircle"
-                        src="http://daisyui.com/tailwind-css-component-profile-2@56w.png"
+                        src={timothy}
                     />
                     <div>
-                        <div className="font-bold">Timothy</div>
+                        <div className="font-bold">Timothy Gracie W</div>
                         <div className="text-sm opacity-50">Timothy</div>
                     </div>
                     </div>
                     <div>
-                    000 000 000000
+                    000 000 45042
                     <br />
                     </div>
                     <Button color="ghost" size="xs">
--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
+import front from '../assets/images/front.png';
 
 type HomeProps = {};
 
@@ -8,7 +9,8 @@ const Home: FC<HomeProps> = (props) => {
     <div className="root h-full w-full items-center justify-center">
       <div className="avatar flex h-full w-full items-center justify-center">
         <div className="w-60 rounded-full">
-          <img src="https://placeimg.com/192/192/people" />
+          <img src= {front}
+          />
         </div>
       </div>
 
