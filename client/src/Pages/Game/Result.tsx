df3242cefd635d21da0f044a09178dfd414fe480
Filbert Mangiri <filbert.mangiri@student.umn.ac.id>
2022-11-27 22:57:56 +0700

    Update Result.tsx

--- client/src/Pages/Game/Result.tsx
+++ client/src/Pages/Game/Result.tsx
@@ -6,11 +6,36 @@ type ResultProps = {
 };
 
 const Result: FC<ResultProps> = (props) => {
+  const result: {
+    percentage: number;
+    diagnosis: 'normal' | 'mild' | 'moderate' | 'severe';
+  } = {
+    percentage: 0,
+    diagnosis: 'normal',
+  };
+
+  const wrongAnswers = 17 - props.correctAnswers;
+
+  if (wrongAnswers <= 3) {
+    result.percentage = (props.correctAnswers / 17) * 100;
+    result.diagnosis = 'normal';
+  } else if (wrongAnswers <= 6) {
+    result.percentage = (props.correctAnswers / 17) * 100;
+    result.diagnosis = 'mild';
+  } else if (wrongAnswers <= 12) {
+    result.percentage = (props.correctAnswers / 17) * 100;
+    result.diagnosis = 'moderate';
+  } else {
+    result.percentage = (props.correctAnswers / 17) * 100;
+    result.diagnosis = 'severe';
+  }
+
   return (
     <div>
       {props.questionNumber ? (
         <div>
-          <span>RESULT : {props.correctAnswers}</span>
+          <h3>Percentage : {result.percentage.toFixed(2)}</h3>
+          <h3>Diagnosis : {result.diagnosis}</h3>
         </div>
       ) : (
         <div>
